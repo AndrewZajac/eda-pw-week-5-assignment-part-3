c84522ca78f7c2027b7bbba5e31abfe3e6db7320
AndrewZajac <andrewzajac05@gmail.com>
2024-04-17 13:03:10 -0500

    Added the 6 record to myCollection, made the addToCollection function work

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,59 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
 
+// let album = title + artist + yearPublished;
+let myCollection = [
+  {
+    title: 'Heartache Medication',
+    artist: "Jon Pardi",
+    yearPublished: 2019 
+  },
+  {
+    title: 'Heartache on the Dance Floor',
+    artist: "Jon Pardi",
+    yearPublished: 2018 
+  },
+  {
+    title: 'White Iverson',
+    artist: "Post Malone",
+    yearPublished: 2016 
+  },
+  {
+    title: 'Black Betty',
+    artist: "Ram Jam",
+    yearPublished: 2004 
+  },
+  {
+    title: 'Pump It Harder',
+    artist: "Black Eyed Peas",
+    yearPublished: 2009 
+  },
+  {
+    title: 'Colder Weather',
+    artist: "Zac Brown Band",
+    yearPublished: 2010 
+  }
+]
 
+function addToCollection(collection, title, artist, yearPublished){
+  let record = {
+      collection,
+      title,
+      artist,
+      yearPublished
+    }
+    collection.push(record);
+    return record;
+}
+
+// myCollection.push(record)
+
+
+console.log(myCollection);
+
+// function showCollection(collection){
+//   for(i === 0; i )
+// }
 
 
 
